04140b51266742574570f0588c8bbe65f57d25d5
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-07-18 18:49:05 -0400

    Added generator

--- templates/template.module.tsx
+++
@@ -1,6 +0,0 @@
-import { default as TemplateComponent, TemplateProps } from './template.component';
-
-// Inject service dependencies here
-export default (props: TemplateProps) => (
-  <TemplateComponent {...props} />
-);
